e5048cd36c07c68ed5ed7da7581d9f46d2f67411
omkar-gits <sangpal.omkar2811@gmail.com>
2025-03-18 09:21:52 +0530

    String Addition

--- Basic_prints.c
+++ Basic_prints.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
-//will be using this file as a main test file
-//Dedicate directories will be made for specific data structures
+
 int main(){
     int a = 95;
-	int *ptr = &a;
-	int **ptr2 = &ptr;
-	int ***ptr3 = &ptr2;
-	printf("This is int a = %d\n", a);
-	printf("This is memory address of a = %p\n",ptr);
-	printf("This is memory address of ptr = %p\n",ptr2);
-	printf("This is memory address of ptr2 = %p\n",ptr3);
-	printf("This is memory address of ptr3 = %p\n",ptr3);
-	printf("This is value at ptr3 %p \n",*ptr3 );
-	printf("This is value at ptr %p \n",*ptr2 );
-	printf("This is value at a %d \n",*ptr);
-	printf("This is value at ptr %p \n",*ptr);
-	printf("This is value at ptr %c \n",*ptr);
+    char name[] = "Omkar";  // String
+    int *ptr = &a;  // Pointer to int
+    int **ptr2 = &ptr;  // Pointer to pointer
+    int ***ptr3 = &ptr2;  // Pointer to pointer to pointer
+
+    // Printing integer and its memory address
+    printf("This is int a = %d\n", a);
+    printf("This is memory address of a = %p\n", (void*)ptr);
+    printf("This is memory address of ptr = %p\n", (void*)ptr2);
+    printf("This is memory address of ptr2 = %p\n", (void*)ptr3);
+    printf("This is memory address of ptr3 = %p\n", (void*)ptr3);
+
+    // Printing values using pointers
+    printf("This is value at ptr3 %p \n", (void*)*ptr3);
+    printf("This is value at ptr2 %p \n", (void*)*ptr2);
+    printf("This is value at ptr %c \n", *ptr);
+
+    // Printing the string correctly
+    printf("This is my name: %s\n", name);
+
+    return 0;
 }
---
+++ Linked_List.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+struct node
+{
+    /* data */
+    int data;
+    struct node* link;
+};
+
+int main(){
+    node *A;    
+    A = NULL;
+}
