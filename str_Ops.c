746bae58598e4278106ea5c5bf5ed38be3f7f95c
omkar-gits <sangpal.omkar2811@gmail.com>
2025-03-27 00:57:08 +0530

    String operations

--- str_Ops.c
+++ str_Ops.c
@@ -8,6 +8,7 @@ const char *ch_str = "Omkar";
 // char ch_str[] = "Omkar"; works because we are declaring a char array.
 // The compiler automatically appends a null terminator '\0' at the end,
 // so it is equivalent to: {'O', 'm', 'k', 'a', 'r', '\0'} stored as an array.
+//the size of string accounts for the null character \0 , it is counted in the length
 //
 // But if we do:
 // char *ch_str[] = "Omkar"; (Actually this does not work anywhere)
@@ -28,6 +29,8 @@ const char *ch_str = "Omkar";
 // But since ch_str is a pointer, dereferencing is required
 // when we want to access individual characters.
 
+char str[] = "helloC";
+char temp_1[1024], temp_2[1024];
 
 int main(){
 	//just basic printing out the default values
@@ -36,5 +39,32 @@ int main(){
 	//printing out using memory address
 	printf("this is value at a memory location %c \n", *ch_str);
 	printf("this is the memory address %p \n", &ch_str);
+	
+	//getting into String operations
+	//strlen(str); return number of character in str with \0;
+	printf("Characters in str %d \n",strlen(str));
+
+	//strcpy(destination , source);
+	strcpy(temp_1,str);
+	printf("The copied string is %s \n",temp_1);
+
+	//strncpy(destination, source , number of chars to be copied);
+	strncpy(temp_2,str,3);
+	printf("this string has copied 3 characters from str %s \n",temp_2);
+
+	//strcat(destination , source); CONCATENATION
+	strcat(temp_2,temp_1);
+	printf("This is the concatenated string %s \n",temp_2);
+
+	//str comparision
+	printf("temp_1 %s \n",temp_1);
+	printf("temp_2 %s \n",temp_2);
+	if(strcmp(temp_2,temp_1) > 0){
+		printf("then %s < %s \n" , temp_1 , temp_2);
+	}
+	if (strcmp(temp_2,temp_1) < 0){
+		printf("then %s > %s \n",temp_1 , temp_2);
+	}
+	
 	return 0;
 }
